d07e5d6b7555f7adf190c897cb33da686031dade
victor mann <victorcmann@gmail.com>
2020-06-10 22:32:11 -0300

    estrutura basica

---
+++ src/AppStyled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+height: 100vh;
+background: red;
+`;
+
+
+export const Menu = styled.div`
+display: flex;
+background: green;
+width: 80px;
+`;
+
+
+export const PageBody = styled.div`
+display: flex;
+flex: 1;
+background: blue;
+`;
