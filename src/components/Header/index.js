bc9d7158e49d19486898d1cd8c00bf76a6edb93b
victor mann <victorcmann@gmail.com>
2020-06-14 14:12:21 -0300

    campo busca

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Logo, SearchInput } from './styled';
 
 export default () => {
     
+    const [inputActive, setInputActive] = useState(false);
+
+    const handleInputFocus = () => {
+        setInputActive(true);
+    };
+
+    const handleInputBlur = e => {
+        let value = e.target.value.trim();
+        if (!value) setInputActive(false);
+    };
+
     return (
         <Container>
             <Logo src="/assets/logo.png" />
-            <SearchInput placeholder="Procure um produto" />
+            <SearchInput 
+                type="search"
+                placeholder="Procure um produto"
+                className={inputActive ? 'active' : ''}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur} />
         </Container>
     );
 }
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -13,4 +13,21 @@ max-width: 100%;
 width: 130px;
 `;
 
-export const SearchInput = styled.input``;
+export const SearchInput = styled.input`
+background: rgba(255, 255, 255, .7) url("/assets/search.png") no-repeat .6em center;
+background-size: auto 2em;
+width: 0;
+padding: 1em 0 1em 3.2em;
+border-radius: 2em;
+outline: none;
+border: none;
+cursor: pointer;
+transition: .2s ease;
+
+&.active {
+    padding-right: 1em;
+    width: 250px;
+    background-color: white;
+    cursor: initial;
+}
+`;
