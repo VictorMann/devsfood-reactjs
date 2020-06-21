d0ddb59dc8154d0c191a5ad035d51bc430a6425e
victor mann <victorcmann@gmail.com>
2020-06-21 14:57:42 -0300

    handleFetch method in api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,10 @@ const BASE = 'https://api.b7web.com.br/devsfood/api';
 
 export default {
     async getCategories() { 
-        let res = await fetch(BASE + '/categories').catch();
+        return await this.handleFetch(BASE + '/categories');
+    },
+    async handleFetch(...args) {
+        let res = await fetch(...args).catch();
         res = res.ok ? await res.json() : {error: res.statusText};
         if (!res.error) res = res.result;
         else alert(res.error);
